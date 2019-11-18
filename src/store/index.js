2e33ecccf410f04e9362307ca19665a4c085f564
Your Name <1282632965@qq.com>
2019-11-18 21:41:25 +0800

    项目初始化

--- src/store/index.js
+++ src/store/index.js
@@ -3,13 +3,16 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+// 导出一个实例
 export default new Vuex.Store({
+  // state生命数据的
   state: {
   },
+  // mutations该数据的
   mutations: {
   },
+  // actions异步拿数据的
   actions: {
-  },
-  modules: {
   }
+
 })
